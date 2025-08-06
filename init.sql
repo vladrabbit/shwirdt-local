1bf7fc2afbcccf86c18aae3525f8cc8cebd4f7f4
vladrabbit <vlad.rabbit@gmail.com>
2025-08-06 16:19:39 +0300

    HW

--- init.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS requests (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    request_ip VARCHAR(45),
-    request_date DATETIME
-);
--- start.sh
+++ start.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-docker compose -f compose.yaml up --build
+# start.sh
+
+set -e
+
+echo "Ожидание MySQL на $DB_HOST..."
+while ! nc -z $DB_HOST 3306; do
+  sleep 1
+done
+
+echo "MySQL доступен. Запускаем uvicorn."
+exec uvicorn main:app --host 0.0.0.0 --port 5000 --lifespan on
